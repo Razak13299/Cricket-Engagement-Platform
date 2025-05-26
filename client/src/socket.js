a5e1c9245df255936813e91eec643feea78c3131
Razak13299 <abdulrazakshaik13299@outlook.com>
2025-05-26 01:50:07 +0100

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://<your-render-backend-url>', {
+const socket = io('https://cricket-backend-uswr.onrender.com', {
   withCredentials: true,
   transports: ['websocket'],
 });
